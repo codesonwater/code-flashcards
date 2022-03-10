a555b5991c842b1a16ed578a5a1d26fe335c9563
Jeremiah Newman <jeremiah.n.a.newman@gmail.com>
2022-03-10 18:58:24 -0500

    formatted arrows

--- src/components/CardSlider.js
+++ src/components/CardSlider.js
@@ -3,21 +3,27 @@ import { useParams } from "react-router-dom";
 import Flashcard from "./Flashcard";
 
 function CardSlider() {
-    const [flashcard, setFlashcard] = useState({});
-    const params = useParams();
-    console.log(flashcard)
+  const [flashcard, setFlashcard] = useState({});
+  const params = useParams();
+  console.log(flashcard);
 
-    useEffect(() => {
-      fetch(`http://localhost:3001/questions/${params.id}`)
-        .then((response) => response.json())
-        .then((data) => setFlashcard(data))
-        .catch(alert);
-    }, [params.id]);
+  useEffect(() => {
+    fetch(`http://localhost:3001/questions/${params.id}`)
+      .then((response) => response.json())
+      .then((data) => setFlashcard(data))
+      .catch(alert);
+  }, [params.id]);
 
   return (
-    <div className="cardSlider">
-      <Flashcard flashcard={flashcard}/>
-    </div>
+    <>
+      <div className="cardSlider">
+        <Flashcard flashcard={flashcard} />
+      </div>
+      <div className="sliderNav">
+        <button className="sliderBtn"> {'◀'} </button>
+        <button className="sliderBtn"> {'►'} </button>
+      </div>
+    </>
   );
 }
 
